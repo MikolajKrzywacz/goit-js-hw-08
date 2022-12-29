f33cb0757ec9c4ed049e770644c1cea4e0886e21
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-29 16:00:50 +0100

    task 2 and 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
+TIME_STAMP = 'time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+const onSaveTimeUpdate = ({ seconds } = 0) => {
+  console.log(seconds);
+  localStorage.setItem(TIME_STAMP, seconds);
+};
+
+const getCuttentVideoPlayerTime = function () {
+  return localStorage.getItem(TIME_STAMP);
+};
+
+player.setCurrentTime(getCuttentVideoPlayerTime());
+player.on('timeupdate', _.throttle(onSaveTimeUpdate, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import _ from 'lodash';
 
+const emailInput = document.querySelector('[name="email"]');
+const messageInput = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
+const throttle = _.throttle;
+
+form.addEventListener(
+  'input',
+  throttle(function () {
+    const state = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(state));
+  }, 500)
+);
+
+// Wypełnij pola formularza danymi z storage
+const storedState = localStorage.getItem('feedback-form-state');
+if (storedState) {
+  const state = JSON.parse(storedState);
+  emailInput.value = state.email;
+  messageInput.value = state.message;
+}
+
+// Wyczyść storage i pola formularza po wysłaniu formularza
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+});
+
+// emailInput.addEventListener(
+//   'input',
+//   _.throttle(() => {
+//     localStorage.setItem('email', `${emailInput.value}`);
+//   }, 500)
+// );
+// messageInput.addEventListener(
+//   'input',
+//   _.throttle(() => {
+//     localStorage.setItem('message', `${messageInput.value}`);
+//   }, 500)
+// );
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+// });
+// console.log(emailInput);
+// console.log(messageInput);
