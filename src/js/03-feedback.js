6454ae829eb2a3c50462653e16cb9507dbecb057
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-29 17:11:30 +0100

    Update 03-feedback.js
    logging in console values of email and message before removing

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -27,11 +27,11 @@ if (storedState) {
 // Wyczyść storage i pola formularza po wysłaniu formularza
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = '';
-  messageInput.value = '';
   console.log({
     email: emailInput.value,
     message: messageInput.value,
   });
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
 });
